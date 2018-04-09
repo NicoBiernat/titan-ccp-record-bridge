da0280033c2b40aefd80520b91752be4e4509a2b
Sören Henning <post@soeren-henning.de>
2018-04-09 15:29:50 +0200

    constant extraction

--- src/main/java/titan/ccp/kiekerbridge/raritan/RaritanJsonTransformer.java
+++ src/main/java/titan/ccp/kiekerbridge/raritan/RaritanJsonTransformer.java
@@ -15,6 +15,15 @@ import titan.ccp.model.PowerConsumptionRecord;
 
 public class RaritanJsonTransformer implements Function<String, List<IMonitoringRecord>> {
 
+	private static final String SENSORS_KEY = "sensors";
+	private static final String DEVICE_KEY = "device";
+	private static final String LABEL_KEY = "label";
+	private static final String ROWS_KEY = "rows";
+	private static final String TIMESTAMP_KEY = "timestamp";
+	private static final String RECORDS_KEY = "records";
+	private static final String AVG_VALUE_KEY = "avgValue";
+	private static final String ID_KEY = "id";
+
 	private static final String RELEVANT_SENSOR_NAME = "activePower";
 
 	private final JsonParser jsonParser = new JsonParser();
@@ -22,19 +31,19 @@ public class RaritanJsonTransformer implements Function<String, List<IMonitoring
 	@Override
 	public List<IMonitoringRecord> apply(final String json) {
 		final JsonObject rootObject = this.jsonParser.parse(json).getAsJsonObject();
-		final JsonArray sensors = rootObject.get("sensors").getAsJsonArray();
+		final JsonArray sensors = rootObject.get(SENSORS_KEY).getAsJsonArray();
 		final int relevantSensorIndex = this.getReleventSensorIndex(sensors);
-		final String sensorLabel = sensors.get(relevantSensorIndex).getAsJsonObject().get("device").getAsJsonObject()
-				.get("label").getAsString();
-		final JsonArray rows = rootObject.get("rows").getAsJsonArray();
+		final String sensorLabel = sensors.get(relevantSensorIndex).getAsJsonObject().get(DEVICE_KEY).getAsJsonObject()
+				.get(LABEL_KEY).getAsString();
+		final JsonArray rows = rootObject.get(ROWS_KEY).getAsJsonArray();
 
 		final List<IMonitoringRecord> monitoringRecords = new ArrayList<>(rows.size());
 		for (final JsonElement rowJsonElement : rows) {
 			final JsonObject row = rowJsonElement.getAsJsonObject();
-			final long timestamp = row.get("timestamp").getAsLong();
-			final JsonArray records = row.get("records").getAsJsonArray();
+			final long timestamp = row.get(TIMESTAMP_KEY).getAsLong();
+			final JsonArray records = row.get(RECORDS_KEY).getAsJsonArray();
 			final JsonObject relevantRecord = records.get(relevantSensorIndex).getAsJsonObject();
-			final int value = (int) relevantRecord.get("avgValue").getAsDouble(); // TODO temp casting to int
+			final int value = (int) relevantRecord.get(AVG_VALUE_KEY).getAsDouble(); // TODO temp casting to int
 
 			monitoringRecords.add(new PowerConsumptionRecord(sensorLabel, timestamp, value));
 		}
@@ -43,7 +52,7 @@ public class RaritanJsonTransformer implements Function<String, List<IMonitoring
 
 	private int getReleventSensorIndex(final JsonArray sensors) {
 		for (int i = 0; i < sensors.size(); i++) {
-			final String sensorName = sensors.get(i).getAsJsonObject().get("id").getAsString();
+			final String sensorName = sensors.get(i).getAsJsonObject().get(ID_KEY).getAsString();
 			if (sensorName.equals(RELEVANT_SENSOR_NAME)) {
 				return i;
 			}
