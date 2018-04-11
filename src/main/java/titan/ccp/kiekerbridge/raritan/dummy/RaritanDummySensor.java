4800ee3d797b13f3ee2b274aca3bfc29fb9c5067
Sören Henning <post@soeren-henning.de>
2018-04-11 14:48:55 +0200

    implemented dummy sensor

---
+++ src/main/java/titan/ccp/kiekerbridge/raritan/dummy/RaritanDummySensor.java
@@ -0,0 +1,47 @@
+package titan.ccp.kiekerbridge.raritan.dummy;
+
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+
+public class RaritanDummySensor {
+
+	private static final URI PUSH_URI = URI.create("localhost");
+
+	private final String sensor;
+
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	private final HttpClient client = HttpClient.newHttpClient();
+
+	public RaritanDummySensor(final String sensor) {
+		this.sensor = sensor;
+	}
+
+	public void run() {
+		final Runnable sender = () -> this.sendMessage(this.generateMessage(10));
+		this.scheduler.scheduleAtFixedRate(sender, 10, 10, TimeUnit.SECONDS);
+	}
+
+	private String generateMessage(final int value) {
+		final long timestamp = System.currentTimeMillis();
+		return MessageFormat.format(JsonTemplate.TEMPLATE, this.sensor, String.valueOf(timestamp),
+				String.valueOf(value));
+	}
+
+	private void sendMessage(final String message) {
+		final HttpRequest request = HttpRequest.newBuilder().uri(PUSH_URI)
+				.POST(HttpRequest.BodyProcessor.fromString(message)).build();
+		this.client.sendAsync(request, HttpResponse.BodyHandler.<Void>discard(null));
+	}
+
+	public static void main(final String[] args) {
+		new RaritanDummySensor("my-sensor").run();
+	}
+
+}
