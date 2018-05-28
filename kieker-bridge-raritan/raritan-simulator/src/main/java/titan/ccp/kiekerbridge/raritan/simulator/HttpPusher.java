9c464ab7f396640024751baf855caad36b16bc4b
Sören Henning <post@soeren-henning.de>
2018-05-28 09:43:57 +0200

    added a warning when http push failed

--- kieker-bridge-raritan/raritan-simulator/src/main/java/titan/ccp/kiekerbridge/raritan/simulator/HttpPusher.java
+++ kieker-bridge-raritan/raritan-simulator/src/main/java/titan/ccp/kiekerbridge/raritan/simulator/HttpPusher.java
@@ -1,6 +1,10 @@
 package titan.ccp.kiekerbridge.raritan.simulator;
 
 import java.net.URI;
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
@@ -8,6 +12,8 @@ import jdk.incubator.http.HttpResponse;
 import jdk.incubator.http.HttpResponse.BodyHandler;
 
 public class HttpPusher {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpPusher.class);
 
 	private final HttpClient client = HttpClient.newHttpClient();
 
@@ -18,9 +24,14 @@ public class HttpPusher {
 	}
 
 	public void sendMessage(final String message) {
-		final HttpRequest request = HttpRequest.newBuilder().uri(this.pushUri)
+		final HttpRequest request = HttpRequest.newBuilder().uri(this.pushUri).timeout(Duration.ofSeconds(10))
 				.POST(HttpRequest.BodyPublisher.fromString(message)).build();
 		final BodyHandler<Void> bodyHandler = HttpResponse.BodyHandler.discard(null);
-		this.client.sendAsync(request, bodyHandler);
+
+		this.client.sendAsync(request, bodyHandler).exceptionally(e -> {
+			LOGGER.warn("Failed to push message.", e);
+			return null;
+		});
+
 	}
 }
