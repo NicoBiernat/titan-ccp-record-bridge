64fba57ce5329b451bdce3d76f3ba8700aec01b1
Sören Henning <post@soeren-henning.de>
2018-07-20 09:16:34 +0200

    Update for scalability evaluation

--- kieker-bridge-raritan/raritan-simulator/src/main/java/titan/ccp/kiekerbridge/raritan/simulator/SimulationRunner.java
+++ kieker-bridge-raritan/raritan-simulator/src/main/java/titan/ccp/kiekerbridge/raritan/simulator/SimulationRunner.java
@@ -1,13 +1,12 @@
 package titan.ccp.kiekerbridge.raritan.simulator;
 
+import java.io.IOException;
 import java.net.URI;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -17,6 +16,10 @@ import org.apache.commons.configuration2.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+import jdk.incubator.http.HttpResponse.BodyHandler;
 import titan.ccp.common.configuration.Configurations;
 
 public class SimulationRunner {
@@ -25,7 +28,7 @@ public class SimulationRunner {
 
 	private final HttpPusher httpPusher;
 
-	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(16);
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(8);
 
 	private final AtomicLong counter = new AtomicLong(0);
 
@@ -36,48 +39,83 @@ public class SimulationRunner {
 		this.sensorReaders = sensors.stream().map(s -> new SensorReader(s)).collect(Collectors.toList());
 	}
 
-	public List<ScheduledFuture<?>> run() {
-		final List<ScheduledFuture<?>> futures = new ArrayList<>(this.sensorReaders.size());
+	public void run() throws IOException {
 		for (final SensorReader sensorReader : this.sensorReaders) {
 			final Runnable sender = () -> {
 				this.httpPusher.sendMessage(sensorReader.getMessage());
 				this.counter.addAndGet(1);
 			};
-			final ScheduledFuture<?> future = this.scheduler.scheduleAtFixedRate(sender, 0,
-					sensorReader.getSensor().getPeroid().toMillis(), TimeUnit.MILLISECONDS);
-			futures.add(future);
+			this.scheduler.scheduleAtFixedRate(sender, 0, sensorReader.getSensor().getPeroid().toMillis(),
+					TimeUnit.MILLISECONDS);
 		}
 
-		// this.scheduler.schedule(() -> {
-		// System.out.println(this.counter.get());
-		// System.exit(0);
-		// }, 10, TimeUnit.SECONDS);
-		return futures;
 	}
 
-	public static void main(final String[] args) throws InterruptedException {
+	public final long getCounter() {
+		return this.counter.get();
+	}
+
+	public void shutdown() {
+		this.scheduler.shutdownNow();
+	}
+
+	public static void main(final String[] args) throws InterruptedException, IOException {
 		// Turn off Java's DNS caching
 		java.security.Security.setProperty("networkaddress.cache.ttl", "0"); // TODO
 
 		final Configuration configuration = Configurations.create();
+		final String setupType = configuration.getString("setup", "scale");
 
-		final String setupType = configuration.getString("setup", "feas");
+		if (setupType.equals("demo")) { // TODO
 
-		List<SimulatedSensor> sensors;
-		if (setupType.equals("scale")) {
 			final int frequency = configuration.getInt("frequency", 1);
 			final int sensorsCount = configuration.getInt("sensors", 1000);
-			sensors = getScalabilitySetup(frequency, sensorsCount, 100);
+			final List<SimulatedSensor> sensors = getScalabilitySetup(frequency, sensorsCount, 100);
 			LOGGER.info("Use scalability setup with frequency: '{}' and sensors: '{}'", frequency, sensorsCount);
+
+			final ScheduledExecutorService monitoringScheduler = Executors.newScheduledThreadPool(1);
+
+			final SimulationRunner runner = new SimulationRunner(
+					URI.create(configuration.getString("kieker.bridge.address")), sensors);
+			runner.run();
+
+			// Start input counter
+			final long startTime = System.currentTimeMillis();
+			monitoringScheduler.scheduleAtFixedRate(() -> {
+				final long elapsedTime = System.currentTimeMillis() - startTime;
+				System.out.println("input;" + elapsedTime + ";" + runner.getCounter());
+			}, 0, 1, TimeUnit.SECONDS);
+
+			// Start output counter
+			final HttpClient httpClient = HttpClient.newHttpClient();
+			monitoringScheduler.scheduleAtFixedRate(() -> {
+				final HttpRequest request = HttpRequest.newBuilder()
+						.uri(URI.create("http://cc01:31302/power-consumption-count")).GET().build();
+				final BodyHandler<String> bodyHandler = HttpResponse.BodyHandler.asString();
+
+				final long requestStartedTime = System.currentTimeMillis() - startTime;
+
+				httpClient.sendAsync(request, bodyHandler).thenApply(r -> Long.parseLong(r.body())).thenAccept(v -> {
+					final long elapsedTime = System.currentTimeMillis() - startTime;
+					// countData.add(new CountData(elapsedTime, v));
+					System.out.println("output;" + requestStartedTime + ";" + elapsedTime + ";" + v);
+				});
+			}, 0, 1, TimeUnit.SECONDS);
+
+			// Wait for termination
+			Thread.sleep(60 * 1000);
+			runner.shutdown();
+			monitoringScheduler.shutdownNow();
+
 		} else if (setupType.equals("demo")) {
-			sensors = getDemoSetup();
 			LOGGER.info("Use demo setup");
+			new SimulationRunner(URI.create(configuration.getString("kieker.bridge.address")), getDemoSetup()).run();
 		} else {
-			sensors = getFeasibilitySetup();
 			LOGGER.info("Use feasability setup");
+			new SimulationRunner(URI.create(configuration.getString("kieker.bridge.address")), getFeasibilitySetup())
+					.run();
 		}
 
-		new SimulationRunner(URI.create(configuration.getString("kieker.bridge.address")), sensors).run();
 	}
 
 	public static List<SimulatedSensor> getFeasibilitySetup() {
