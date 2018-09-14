190aecdcee0f639f0d57b4c2972875f5d2973c4b
Sören Henning <post@soeren-henning.de>
2018-09-14 10:34:10 +0200

    Log number send record to Redis

--- load-tester-bridge/src/main/java/titan/ccp/kiekerbridge/loadtester/LoadTester.java
+++ load-tester-bridge/src/main/java/titan/ccp/kiekerbridge/loadtester/LoadTester.java
@@ -6,10 +6,12 @@ import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.jctools.queues.MpscArrayQueue;
 
 import kieker.common.record.IMonitoringRecord;
+import redis.clients.jedis.Jedis;
 import titan.ccp.kiekerbridge.KiekerBridge;
 import titan.ccp.kiekerbridge.KiekerBridgeStream;
 import titan.ccp.models.records.ActivePowerRecord;
@@ -25,18 +27,41 @@ public class LoadTester {
 		final Random random = new Random();
 		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(8);
 		final Queue<IMonitoringRecord> queue = new MpscArrayQueue<>(1024); // Non-blocking, but lock-free
+		final AtomicLong counter = new AtomicLong(0);
+		final String redisHost = Objects.requireNonNullElse(System.getenv("REDIS_HOST"), "localhost");
+		final int redisPort = Integer.parseInt(Objects.requireNonNullElse(System.getenv("REDIS_PORT"), "6379"));
+		final Jedis jedis = new Jedis(redisHost, redisPort);
 
 		for (int i = 0; i < producers; i++) {
 			final long initialDelay = random.nextInt(periodeInMs);
 			final int id = i; // Make available in following lambda
-			scheduler.scheduleAtFixedRate(
-					() -> queue.add(new ActivePowerRecord("sensor" + id, System.currentTimeMillis(), value)),
-					initialDelay, periodeInMs, TimeUnit.MILLISECONDS);
+			scheduler.scheduleAtFixedRate(() -> {
+				queue.add(new ActivePowerRecord("sensor" + id, System.currentTimeMillis(), value));
+				counter.incrementAndGet();
+			}, initialDelay, periodeInMs, TimeUnit.MILLISECONDS);
 		}
 
 		final KiekerBridgeStream<IMonitoringRecord> stream = KiekerBridgeStream.from(queue);
 		final KiekerBridge kiekerBridge = KiekerBridge.ofStream(stream).build();
 		kiekerBridge.start();
+
+		scheduler.scheduleAtFixedRate(() -> {
+			final long oldValue = counter.getAndSet(0);
+			System.out.println(oldValue);
+			jedis.incrBy("input_counter", oldValue);
+		}, 1, 1, TimeUnit.SECONDS);
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			scheduler.shutdown();
+			try {
+				scheduler.awaitTermination(10, TimeUnit.SECONDS);
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
+			final long oldValue = counter.getAndSet(0);
+			System.out.println(oldValue);
+			jedis.incrBy("input_counter", oldValue);
+		}));
 	}
 
 }
